68a8b1c31c69933bf7b5efb28b9e40b3465fce89
Milos Jovanovic <jovanovicmilos.kg@gmail.com>
2020-03-19 10:00:01 +0100

    Create todos component

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.sass']
+})
+export class TodosComponent implements OnInit {
+  public todos: Todo[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false
+      }
+    ];
+  }
+
+}
